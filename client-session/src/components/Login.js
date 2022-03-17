60dcc5f733ca27f09bfaf2b4440fba73f929ab54
YOON <50129701+yoondiver@users.noreply.github.com>
2022-03-17 23:50:49 +0900

    auth

--- client-session/src/components/Login.js
+++ client-session/src/components/Login.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
     };
     this.inputHandler = this.inputHandler.bind(this);
     this.loginRequestHandler = this.loginRequestHandler.bind(this);
@@ -24,31 +25,58 @@ class Login extends Component {
     //
     // 사용자 정보를 받아온 후
     // - props로 전달받은 함수를 호출해, 사용자 정보를 변경하세요.
+
+    axios
+      .post(
+        "https://localhost:4000/users/login",
+        {
+          userId: this.state.username,
+          password: this.state.password,
+        },
+        {
+          "Content-Type": "application/json",
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        this.props.loginHandler(true);
+        return axios.get("https://localhost:4000/users/userinfo", {
+          withCredentials: true,
+        });
+      })
+      .then((res) => {
+        let { userId, email } = res.data.data;
+        this.props.setUserInfo({
+          userId,
+          email,
+        });
+      })
+      .catch((err) => alert(err));
   }
 
   render() {
     return (
-      <div className='loginContainer'>
-        <div className='inputField'>
+      <div className="loginContainer">
+        <div className="inputField">
           <div>Username</div>
           <input
-            name='username'
+            name="username"
             onChange={(e) => this.inputHandler(e)}
             value={this.state.username}
-            type='text'
+            type="text"
           />
         </div>
-        <div className='inputField'>
+        <div className="inputField">
           <div>Password</div>
           <input
-            name='password'
+            name="password"
             onChange={(e) => this.inputHandler(e)}
             value={this.state.password}
-            type='password'
+            type="password"
           />
         </div>
-        <div className='passwordField'>
-          <button onClick={this.loginRequestHandler} className='loginBtn'>
+        <div className="passwordField">
+          <button onClick={this.loginRequestHandler} className="loginBtn">
             Login
           </button>
         </div>
